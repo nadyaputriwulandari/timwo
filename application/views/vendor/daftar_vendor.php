38188c65a4e26749f630c35f3f83966ae6f77bea
annisaapriyeni <annisa18ti@mahasiswa.pcr.ac.id>
2021-07-08 00:25:44 +0700

    test

--- application/views/vendor/daftar_vendor.php
+++ application/views/vendor/daftar_vendor.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V18</title>
+	<title>Daftar</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="<?= base_url(''); ?>assets/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/select2/select2.min.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(''); ?>assets/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body style="background-color: #666666;">
@@ -32,33 +32,33 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form"  method="POST" action="<?php echo site_url('vendor/daftar_vendor/input_aksi')?>">
 					<span class="login100-form-title p-b-43">
 						Registrasi Vendor
 					</span>
 					
 					
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email">
+						<input class="input100" type="text" name="nama_vendor">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Nama Vendor</span>
 					</div>
 					
 					
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="pass">
+					<div class="wrap-input100 validate-input" >
+						<input class="input100" type="text" name="kategori_vendor">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Kategori</span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="pass">
+					<div class="wrap-input100 validate-input" >
+						<input class="input100" type="text" name="no_telp_vendor">
 						<span class="focus-input100"></span>
 						<span class="label-input100">No Hp</span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="pass">
+					<div class="wrap-input100 validate-input" >
+						<input class="input100" type="text" name="alamat_vendor">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Alamat</span>
 					</div>
@@ -81,7 +81,7 @@
 						</div>
 
 						<div>
-							<a href="#" class="txt1">
+							<a href="<?php echo base_url('admin/auth');?>" class="txt1">
 								Sudah Daftar? Login Disini
 							</a>
 						</div>
@@ -89,7 +89,7 @@
 			
 
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						<button class="login100-form-btn",>
 							Daftar
 						</button>
 					</div>
