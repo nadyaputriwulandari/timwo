d6bebb9001ba26ed098cb8b0ecd859fa778d68aa
nadyaputriwulandari <52444673+nadyaputriwulandari@users.noreply.github.com>
2021-07-08 01:45:04 +0700

    up

--- application/controllers/user/keranjang.php
+++ application/controllers/user/keranjang.php
@@ -24,6 +24,6 @@ class keranjang extends CI_Controller
 		// die();
 		$this->load->view('user/header');
 		$this->load->view('user/keranjang');
-		$this->load->view('user/footer');
+		//$this->load->view('user/footer');
 	}
 }
